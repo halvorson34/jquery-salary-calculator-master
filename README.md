06eaf295efadd727dd84655045417cabab63362b
Matt Halvorson <halvorson34.mh@gmail.com>
2020-03-15 23:19:51 -0500

    updated read me

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Project Name
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+Salary Calculator
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+The salary calculator takes various inputs of employee data and logs them into a table.
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+The objective is to neatly organize employee data while also providing a tool to compute total monthly salary.
--- Scripts/client.js
+++ Scripts/client.js
@@ -5,17 +5,52 @@ const employees = [];
 $(document).ready(init);
 
 function init() {
-  $(".js-btnSubmit").on("submit", addEmployee);
+  $(".js-btnSubmit").on("click", addEmployee);
   $(".btnDelete").on("click", deleteEmployee);
+}
+
+function deleteEmployee() {
+  const employeeIndex = $(this).data("index");
+
+  employees.splice(employeeIndex, 1);
+
+  render();
 }
 
 function addEmployee(first, last, id, title, salary) {
   event.preventDefault();
 
-const employee = {
-  first: $(".firstNameInput").val(),
-  last: $(".lastNameInput").val(),
-  id: $(".idInput").val(),
-  title: $(".titleInput").val(),
-  salary: $(".salaryInput").val(),
-};
+  const employee = {
+    first: $(".firstNameInput").val(),
+    last: $(".lastNameInput").val(),
+    id: $(".idInput").val(),
+    title: $(".titleInput").val(),
+    salary: parseFloat($(".salaryInput").val())
+  };
+
+  employees.push(employee);
+  render();
+}
+
+function render() {
+  $(".tableBody").empty();
+  let totalMonthly = 0;
+
+  for (let i = 0; 1 < employees.length; i++) {
+    const individualEmployee = employees[i];
+    totalMonthly += individualEmployee.salary;
+
+    $(".tableBody").append(`
+    <tr>
+        <td>${individualEmployee.first}</td>
+        <td>${individualEmployee.last}</td>
+        <td>${individualEmployee.id}</td>
+        <td>${individualEmployee.title}</td>
+        <td>${individualEmployee.salary}</td>
+        <td><button class="btnDelete" data-index="${i}">Delete</button></td>
+    </tr>
+        `);
+  }
+
+  $(".totalMonthly").text(`${totalMonthly}`);
+}
